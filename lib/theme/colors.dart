ac12dfe2ca5b1b3c69a7a5c3ba6fd814af078392
hosseinvejdani <vejdani.shojae@gmail.com>
2022-11-25 20:52:13 +0330

    complete all parts

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
   static const green = Colors.green;
-  static const red = Colors.redAccent;
-  static const blue = Colors.blueAccent;
-  static const purple = Colors.purpleAccent;
+  // static const red = Colors.redAccent;
+  static const red = Color.fromARGB(255, 164, 80, 203);
 }
